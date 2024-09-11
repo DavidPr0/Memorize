54c6a62ff93f7b1c4d283fab95564c9d0dd828be
DavidPr0 <davidpaco2.4@gmail.com>
2024-09-11 19:02:02 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by David Freitas on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
